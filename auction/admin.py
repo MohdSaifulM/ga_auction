232f8f0bc435f9ff32ef7a0c5ea1b8b43ef1a39e
Schnecke <eiweala@live.com>
2020-12-04 11:51:18 +0800

    fixed bugs

--- auction/admin.py
+++ auction/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Category
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    # tuple
+    list_display = ("id", "name")
+
+
+# register to admin panel
+admin.site.register(Category, CategoryAdmin)
--- auction/views.py
+++ auction/views.py
@@ -1,10 +1,18 @@
+from auction.models import Category
 from django.shortcuts import render
+from django import forms
+
+
+#  form information
+class NewCategoryForm(forms.Form):
+    name = forms.CharField(min_length=4, max_length=60)
+
 
 # Create your views here.
-
-
 def index(request):
-    return render(request, 'auction/index.html')
+    categories = Category.objects.all()
+
+    return render(request, 'auction/index.html', {"categories": categories, "form": NewCategoryForm()})
 
 
 def show(request):
